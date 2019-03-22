6808f6f03fdc4ff7bb42871672aedc6381222654
Rajat Venkatesh <vrajat@users.noreply.github.com>
2019-03-22 09:00:13 +0530

    new:usr:Add support for Name and Location (#4)
    Use spacy (spacy.io) for Named Entity Recognition. It uses
    'en_core_web_sm' model and has to be downloaded separately.

--- piicatcher/scanner.py
+++ piicatcher/scanner.py
@@ -1,6 +1,7 @@
 """Different types of scanners for PII data"""
 from abc import ABC, abstractmethod
 from commonregex import CommonRegex
+import spacy
 
 from piicatcher.piitypes import PiiTypes
 
@@ -31,3 +32,23 @@ class RegexScanner(Scanner):
             types.append(PiiTypes.ADDRESS)
 
         return types
+
+
+class NERScanner(Scanner):
+    """A scanner that uses Spacy NER for entity recognition"""
+    def __init__(self):
+        self.nlp = spacy.load('en_core_web_sm')
+
+    def scan(self, text):
+        """Scan the text and return an array of PiiTypes that are found"""
+        doc = self.nlp(text)
+        types = set()
+        for ent in doc.ents:
+            print(ent.label_)
+            if ent.label_ == 'PERSON':
+                types.add(PiiTypes.PERSON)
+
+            if ent.label_ == 'GPE':
+                types.add(PiiTypes.LOCATION)
+
+        return list(types)
