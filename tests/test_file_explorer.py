8941b85e1627a22459028e4a5e789274edb31822
Rajat Venkatesh <vrajat@dblint.io>
2019-12-18 22:38:32 +0530

    fix: Fix when a single file is specified. Also fix json output for files
    Fix #33 and #34

--- tests/test_file_explorer.py
+++ tests/test_file_explorer.py
@@ -17,19 +17,51 @@ class TestDispatcher(TestCase):
 
 
 class TestExplorer(TestCase):
-    def test_tabular(self):
-        explorer = FileExplorer("/tmp")
+
+    @classmethod
+    def setUpClass(cls):
+        cls.explorer = FileExplorer("/tmp")
         f1 = File("/tmp/1", "text/plain")
         f1._pii.add(PiiTypes.BIRTH_DATE)
 
         f2 = File("/tmp/2", "application/pdf")
         f2._pii.add(PiiTypes.UNSUPPORTED)
 
-        explorer._files.append(f1)
-        explorer._files.append(f2)
+        cls.explorer._files.append(f1)
+        cls.explorer._files.append(f2)
 
+    def test_tabular(self):
         self.assertEqual([
             ['/tmp/1', 'text/plain', '[{"__enum__": "PiiTypes.BIRTH_DATE"}]'],
             ['/tmp/2', 'application/pdf', '[{"__enum__": "PiiTypes.UNSUPPORTED"}]']
-        ], explorer.get_tabular())
+        ], self.explorer.get_tabular())
+
+    def test_json(self):
+        self.assertEqual({
+            'files': [
+                {'Mime/Type': 'text/plain', 'path': '/tmp/1', 'pii': [PiiTypes.BIRTH_DATE]},
+                {'Mime/Type': 'application/pdf', 'path': '/tmp/2',  'pii': [PiiTypes.UNSUPPORTED]}
+            ]
+        }, self.explorer.get_dict())
+
+
+class TestWalker(TestCase):
+
+    def _check(self, result):
+        self.assertEqual(len(result['files']), 1)
+        self.assertEqual(result['files'][0]['Mime/Type'], 'text/plain')
+        self.assertEqual(result['files'][0]['path'], 'tests/samples/sample-data.csv')
+        self.assertEqual(len(result['files'][0]['pii']), 5)
+
+    def testDirectory(self):
+        explorer = FileExplorer("tests/samples")
+        explorer.scan()
+        result = explorer.get_dict()
+        self._check(result)
+
+    def testFile(self):
+        explorer = FileExplorer("tests/samples/sample-data.csv")
+        explorer.scan()
+        result = explorer.get_dict()
+        self._check(result)
 
